7e398c9df7a7f493139395fb14b74c0ec45e5c95
Arav-R <24rawata@smtexas.org>
2023-09-21 12:51:21 -0500

    finished will's team code
    needs servo values and motor directions

---
+++ Tutorials/Arav/ServoUtil.h
@@ -0,0 +1,27 @@
+
+// ServoToggleFunction.c
+
+void toggleServo(int port, int startPos, int endPos, bool &servoState, TVexJoysticks toggleButton)
+{
+    if (vexRT[toggleButton])
+    {
+        if (!servoState)
+        {
+            motor[port] = endPos;
+            servoState = true;
+        }
+        else
+        {
+            motor[port] = startPos;
+            servoState = false;
+        }
+    }
+}
+
+void moveServo(int servoPort, int targetPosition, TVexJoysticks button) {
+    // Check if the button is pressed
+    if (vexRT[button] == 1) {
+        // Set the servo to the target position
+        motor[servoPort] = targetPosition;
+    }
+}
