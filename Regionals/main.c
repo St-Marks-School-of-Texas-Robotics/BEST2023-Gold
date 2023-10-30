bb9bbb757ce13d906e6d84f451f9cdcf1a82c2b6
Arav-R <63694299+Arav-R@users.noreply.github.com>
2023-10-30 16:16:16 -0500

    arm buttons mapped

--- Regionals/main.c
+++ Regionals/main.c
@@ -1,13 +1,34 @@
-#pragma config(Motor,  port2,           leftMotor,     tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port3,           rightMotor,    tmotorVex393_MC29, openLoop)
-
+#pragma config(Sensor, in1, potentiometer, sensorAnalog)
+#pragma config(Motor, port9, armMotor, tmotorServoContinuousRotation, openLoop, reversed)
+#pragma config(Sensor, dgtl3, irLeft, sensorDigitalIn)
+#pragma config(Sensor, dgtl4, irRight, sensorDigitalIn)
+#pragma config(Motor, port2, leftDrive, tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor, port3, rightDrive, tmotorServoContinuousRotation, openLoop, reversed)
 
 #include "Subsystems/drivetrain.h"
+#include "Subsystems/arm.h"
 
 task main()
 {
   while (true)
   {
+  	updateArm(); // Update the arm position using the PID controller
+
+  	if (vexRT[Btn8U]) {
+      setArmSetpoint(1500); // Vein Score
+    } else if (vexRT[Btn8L]) {
+      setArmSetpoint(1500); // Vein Pickup
+    } else if (vexRT[Btn8R]) {
+      setArmSetpoint(1500); // High Artery
+    } else if (vexRT[Btn8D]) {
+      setArmSetpoint(1500); // Low Artery
+    }
+
+  	// Drive
     arcadeDrive(vexRT[Ch3], vexRT[Ch1], vexRT[Btn5U]);
+
+
+
+
   }
 }
